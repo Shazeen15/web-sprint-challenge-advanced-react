0cc492640169886936ca0925f3e63c9f1a62049c
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-15 11:11:15 -0800

    set up header test

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  // arrange
+  render(<CheckoutForm />);
+  //   act
+  //   getting the header element
+  const formHeader = screen.getByText(/checkout form/i);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  // arrange
+  render(<CheckoutForm />);
+});
