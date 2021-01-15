7507b4d99e8474cec7dcb7551b67dcbb0c1373e0
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-15 14:49:56 -0800

    added assert to testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -16,4 +16,30 @@ test("form header renders", () => {
 test("form shows success message on submit with form details", () => {
   // arrange
   render(<CheckoutForm />);
+
+  //   act
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const addressInput = screen.getByLabelText(/address/i);
+  const cityInput = screen.getByLabelText(/city/i);
+  const stateInput = screen.getByLabelText(/state/i);
+  const zipInput = screen.getByLabelText(/zip/i);
+  const button = screen.getByRole("button");
+
+  //   typing in the values
+  userEvent.type(firstNameInput, "shazeen");
+  userEvent.type(lastNameInput, "fabius");
+  userEvent.type(addressInput, "4903 wiltsie way");
+  userEvent.type(cityInput, "san diego");
+  userEvent.type(stateInput, "california");
+  userEvent.type(zipInput, "92124");
+
+  //   click the checkout buttong
+  userEvent.click(button);
+
+  //   assert
+  const newFirstNameInput = screen.findByText("shazeen");
+  newFirstNameInput.then((firstName) => {
+    expect(firstName).toBeInTheDocument();
+  });
 });
